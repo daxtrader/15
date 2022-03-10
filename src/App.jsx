ddbc1e855b187f0f3b5d8e2765847712c8951bd4
daxtrader <47599146+daxtrader@users.noreply.github.com>
2022-03-10 20:48:41 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ import Text from "antd/lib/typography/Text";
 import Ramper from "components/Ramper";
 import MenuItems from "./components/MenuItems";
 const { Header, Footer } = Layout;
-// Seriously
+// Seriously this is a joke
 const styles = {
   content: {
     display: "flex",
